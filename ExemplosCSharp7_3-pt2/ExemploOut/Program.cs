96d582b5bd4c8bf530a9938b35807ead874287ab
Renato Groffe <renato.groffe@yahoo.com.br>
2018-07-14 23:58:48 -0300

    C# 7.3 - Novos Recursos
    C# 7.3 - Novos Recursos

---
+++ ExemplosCSharp7_3-pt2/ExemploOut/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ExemploOut
+{
+    public class ClassePai
+    {
+        public int Valor1 { get; set; }
+
+        public ClassePai(int v1, out int v2)
+        {
+            Valor1 = v1;
+            v2 = v1 * 2;
+        }
+    }
+
+    public class ClasseFilha : ClassePai
+    {
+        public ClasseFilha(int v1) : base(v1, out _)
+        {
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var teste = new ClasseFilha(10);
+            Console.WriteLine($"Valor1 = {teste.Valor1}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ExemplosCSharp7_3-pt2/ExemploSobrecargaIn/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ExemploSobrecargaIn
+{
+    class Program
+    {
+        private static void TesteSobrecarga(int valor)
+        {
+            Console.WriteLine($"Sem o modificador in: {valor}");
+        }
+
+        public static void TesteSobrecarga(in int valor)
+        {
+            Console.WriteLine($"Com o modificador in: {valor}");
+        }
+
+        static void Main(string[] args)
+        {
+            TesteSobrecarga(5);
+
+            int valorTeste = 10;
+            TesteSobrecarga(in valorTeste);
+
+            Console.ReadKey();
+        }
+    }
+}
